cd32c76a1baf1a47f4e0ec7ea6778fba8ae74773
MARCEL SANT ANA DOS ANJOS <47478118852@info.local>
2026-03-05 21:46:07 -0300

    Funcao mae & funcao mse

--- calc.dados.py
+++ calc.dados.py
@@ -3,7 +3,14 @@ def validar_vetores(v1,v2):
         raise ArithmeticError("Os vetores devem ter o mesmo tamanho")
     return True
 
+def mae ( v1 , v2 ) :
+    validar_vetores ( v1 , v2 )
+    return sum( abs( a - b ) for a , b in zip ( v1 , v2 ) ) / len ( v1 )
+def mse ( v1 , v2 ) :
+    validar_vetores ( v1 , v2 )
+    return sum (( a - b ) ** 2 for a , b in zip ( v1 , v2 ) ) / len ( v1 )
+
 if __name__ == "__main__":
     obs = [1.2, 2.5, 3.8]
-    pred = [1.0, 2.7, 4.0, 6.0]
-    print("Sistema iniciado com sucesso")
+    pred = [1.0, 2.7, 4.0]
+    print(mae(obs,pred))
